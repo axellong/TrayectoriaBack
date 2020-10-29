4b4439ce3b5b0615b94e013654cfbdfbf90d18e6
axellong <193250@ids.upchiapas.edu.mx>
2020-10-29 01:39:38 -0600

    add python.decouple

--- TrayectoriaBack/settings.py
+++ TrayectoriaBack/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^r=o&58_*rd24oh#ivny-k&7x(^6!ydr=o^oi+x3%4&c=l*v1$'
+SECRET_KEY=config("SECRET_KEY")'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = config('DEBUG',default = False, cast = bool)
 
 ALLOWED_HOSTS = ['back-web-1.herokuapp.com']
 
