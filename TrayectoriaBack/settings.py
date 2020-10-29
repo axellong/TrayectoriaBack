a8ea361e7248f11e166a3169df1e1d114c86dfc0
axellong <193250@ids.upchiapas.edu.mx>
2020-10-29 01:24:55 -0600

    add git ignore

--- TrayectoriaBack/settings.py
+++ TrayectoriaBack/settings.py
@@ -162,3 +162,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from TrayectoriaBack. local_settings import*
+except ImportError:
+    pass
