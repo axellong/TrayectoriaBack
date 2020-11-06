a30cd3f2565b59d4be502d153f0b48b3b0cf3384
axellong <193250@ids.upchiapas.edu.mx>
2020-11-06 05:37:44 -0600

    databse ready

--- TrayectoriaBack/settings.py
+++ TrayectoriaBack/settings.py
@@ -99,18 +99,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'TrayectoriaBack.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME':'trayectoriadb',
-#         'USER': 'postgres',
-#         'PASSWORD': 'Acoco12345',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432'
-#     }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
